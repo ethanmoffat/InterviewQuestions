76575aa50bdab31288ea68bd3f3cb8455467776b
Ethan Moffat <ethanmoffat@hotmail.com>
2017-08-28 12:01:44 +0200

    Implement first pass of algorithm to find local maximum differences

--- StockProblem/StockProblem/main.cpp
+++ StockProblem/StockProblem/main.cpp
@@ -9,6 +9,7 @@ struct BuySell
 	int SellIndex;
 
 	BuySell() : BuyIndex(0), SellIndex(0) { }
+	BuySell(int buy, int sell) : BuyIndex(buy), SellIndex(sell) { }
 };
 
 std::list<BuySell> FindBestDays(std::shared_ptr<int> inputStockPrices, int numberOfPrices);
@@ -36,10 +37,12 @@ int main(int argc, char * argv[])
 
 		auto buySellPairs = FindBestDays(stocksForThisTestCase, numberOfStockDays);
 		if (buySellPairs.size() == 0)
-			std::cout << "No Profit" << std::endl;
+			std::cout << "No Profit";
 
 		for (const auto& buySell : buySellPairs)
-			std::cout << "(" << buySell.BuyIndex << ", " << buySell.SellIndex << ") ";
+			std::cout << "(" << buySell.BuyIndex << " " << buySell.SellIndex << ") ";
+
+		std::cout << std::endl;
 	}
 
 	inputFile.close();
@@ -51,10 +54,29 @@ std::list<BuySell> FindBestDays(std::shared_ptr<int> inputStockPrices, int numbe
 {
 	std::list<BuySell> retList;
 
-	for (int i = 0; i < numberOfPrices; ++i)
-	{
+	if (numberOfPrices < 2)
+		return retList;
 
+	auto localMin = 0;
+	auto localMax = 0;
+	auto ptr = inputStockPrices.get();
+
+	for (int i = 1; i < numberOfPrices; ++i)
+	{
+		if (ptr[i] < ptr[localMax] && localMax - localMin > 0)
+		{
+			retList.push_back(BuySell(localMin, localMax));
+			localMin = localMax = i;
+		}
+		else if (ptr[i] > ptr[localMax])
+			localMax = i;
+
+		if (ptr[i] < ptr[localMin])
+			localMin = i;
 	}
+
+	if (localMax > 0)
+		retList.push_back(BuySell(localMin, localMax));
 
 	return retList;
 }
