bb985385d21495813026d1a5e6659911b4c7313b
Ethan Moffat <ethanmoffat@hotmail.com>
2017-08-28 11:30:42 +0200

    Add stubbed file I/O and algorithm method

--- StockProblem/StockProblem/main.cpp
+++ StockProblem/StockProblem/main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <memory>
+
+struct BuySell
+{
+	int BuyIndex;
+	int SellIndex;
+
+	BuySell() : BuyIndex(0), SellIndex(0) { }
+};
+
+std::list<BuySell> FindBestDays(std::shared_ptr<int> inputStockPrices, int numberOfPrices);
 
 int main(int argc, char * argv[])
 {
+	std::ifstream inputFile("input.txt");
+	if (inputFile.bad() || inputFile.eof())
+	{
+		std::cout << "Unable to open input.txt" << std::endl;
+		return 1;
+	}
+
+	int numberOfTestCases = 0;
+	inputFile >> numberOfTestCases;
+
+	for (int i = 0; i < numberOfTestCases; ++i)
+	{
+		int numberOfStockDays = 0;
+		inputFile >> numberOfStockDays;
+
+		std::shared_ptr<int> stocksForThisTestCase(new int[numberOfStockDays]);
+		for (int j = 0; j < numberOfStockDays; ++j)
+			inputFile >> stocksForThisTestCase.get()[j];
+
+		auto buySellPairs = FindBestDays(stocksForThisTestCase, numberOfStockDays);
+		if (buySellPairs.size() == 0)
+			std::cout << "No Profit" << std::endl;
+
+		for (const auto& buySell : buySellPairs)
+			std::cout << "(" << buySell.BuyIndex << ", " << buySell.SellIndex << ") ";
+	}
+
+	inputFile.close();
+
 	return 0;
+}
+
+std::list<BuySell> FindBestDays(std::shared_ptr<int> inputStockPrices, int numberOfPrices)
+{
+	std::list<BuySell> retList;
+
+	for (int i = 0; i < numberOfPrices; ++i)
+	{
+
+	}
+
+	return retList;
 }
