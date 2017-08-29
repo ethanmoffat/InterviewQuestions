e4630405baad6838780bac1668b9df07cbc34b39
Ethan Moffat <ethanmoffat@hotmail.com>
2017-08-29 10:58:19 +0200

    Move list code into header file

--- ReverseLinkedList/ReverseLinkedList/list.h
+++ ReverseLinkedList/ReverseLinkedList/list.h
@@ -10,10 +10,62 @@ typedef struct node
 	int value;
 } Node;
 
-PNode CreateList(const std::shared_ptr<int>&  inputArray, int numberOfItems);
+PNode CreateList(const std::shared_ptr<int>&  inputArray, int numberOfItems)
+{
+	if (numberOfItems < 1)
+		return nullptr;
 
-PNode ReverseList(PNode head);
+	PNode head = new Node;
+	PNode current = head;
+	for (int i = 0; i < numberOfItems; ++i)
+	{
+		current->next = i + 1 == numberOfItems ? nullptr : new Node;
+		current->value = inputArray.get()[i];
+		current = current->next;
+	}
 
-void PrintList(PNode head);
+	return head;
+}
 
-void FreeList(PNode head);
+PNode ReverseList(PNode head)
+{
+	if (head == nullptr || head->next == nullptr)
+		return head;
+
+	PNode current = head;
+	PNode previous = nullptr;
+
+	while (current != nullptr)
+	{
+		PNode temp = current;
+		current = current->next;
+
+		temp->next = previous;
+
+		previous = temp;
+	}
+
+	return previous;
+}
+
+void PrintList(PNode head)
+{
+	PNode current = head;
+	while (current != nullptr)
+	{
+		std::cout << current->value << " ";
+		current = current->next;
+	}
+	std::cout << std::endl;
+}
+
+void FreeList(PNode head)
+{
+	PNode current = head;
+	while (current != nullptr)
+	{
+		PNode tmp = current;
+		current = current->next;
+		delete tmp;
+	}
+}
